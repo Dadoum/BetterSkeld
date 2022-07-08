d1a4d3c0980d396cf2ed2a7fc8f9f5166e5cedf4
Dadoum <contact@dadoum.ml>
2022-07-08 23:25:22 +0200

    Even better Skeld

--- Plugin.cs
+++ Plugin.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq;
 using BepInEx;
 using BepInEx.IL2CPP;
 using HarmonyLib;
 using TMPro;
+using UnhollowerRuntimeLib;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
 
 namespace BetterSkeld
 {
@@ -12,53 +15,101 @@ namespace BetterSkeld
     public class BetterSkeldPlugin : BasePlugin
     {
         public static BetterSkeldPlugin Instance { get; private set; }
-        private static bool patched;
 
-        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.Awake))]
+        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.Start))]
         public static class ShipStatusAwakePatch
         {
             public static void Prefix(ShipStatus __instance)
             {
-                patched = __instance.Type != ShipStatus.MapType.Ship;
+                var patcher = new GameObject("BetterSkeld Patcher");
+                switch (__instance.Type)
+                {
+                    case ShipStatus.MapType.Ship:
+                        patcher.AddComponent<SkeldPatcher>();
+                        break;
+                    case ShipStatus.MapType.Hq:
+                        patcher.AddComponent<MiraHQPatcher>();
+                        break;
+                }
             }
         }
 
-        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.FixedUpdate))]
-        public static class ShipStatusUpdatePatch
+        public class SkeldPatcher : MonoBehaviour
         {
-            public static void Postfix()
+            private void FixedUpdate()
             {
-                if (!patched)
+                var client = AmongUsClient.Instance;
+                
+                // On charge Polus 
+                var res = Addressables.LoadAssetAsync<GameObject>(client.ShipPrefabs[(Index) (int) ShipStatus.MapType.Pb]).Result;
+                if (!res)
+                    return; // On réessaiera de charger à la prochaine update
+                
+                Instance.Log.LogDebug("Patching TheSkeld...");
+                
+                // Récup les ressources
+                var vitalsObj = res.transform.Find("Office/panel_vitals").gameObject;
+                var admin = GameObject.Find("MapRoomConsole");
+                var animation = GameObject.Find("MapAnimation");
+                var impostorDetectors = GameObject.FindObjectsOfType<ImpostorDetector>();
+                
+                // Ajouter les signaux vitaux
+                Instance.Log.LogDebug("Placing vitals...");
+                var vitals = GameObject.Instantiate(vitalsObj);
+                vitals.transform.position = new Vector3(1.9162f, -16.1985f, -2.4142f);
+                vitals.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                vitals.transform.localScale = new Vector3(0.6636f, 0.7418f, 1f);
+                
+                // Désactiver l'admin
+                Instance.Log.LogDebug("Disabling admin...");
+                admin.GetComponent<CircleCollider2D>().enabled = false;
+                
+                // Désactiver l'animation car il n'y a plus de carte, donc pour le RP faut plus qu'on la voie
+                animation.active = false;
+                
+                // HACK: je sais pas trop à quoi il sert en temps normal, mais ce composant cause le bug de glissement
+                // à la tâche des feuilles !
+                Instance.Log.LogDebug("Fixing \"Clean O₂ Filter\" task...");
+                foreach (var impostorDetector in impostorDetectors)
                 {
-                    var client = AmongUsClient.Instance;
-                    
-                    // On charge Polus 
-                    var res = Addressables.LoadAssetAsync<GameObject>(client.ShipPrefabs[(Index) (int) ShipStatus.MapType.Pb]).Result;
-                    if (!res)
-                        return; // On réessaiera de charger à la prochaine update
-                    
-                    Instance.Log.LogDebug("Patching TheSkeld...");
-                    
-                    // Récup les ressources
-                    var vitalsObj = res.transform.Find("Office/panel_vitals").gameObject;
-                    var admin = GameObject.Find("MapRoomConsole");
-                    var animation = GameObject.Find("MapAnimation");
-                    
-                    // Ajouter les signaux vitaux
-                    Instance.Log.LogDebug("Placing vitals...");
-                    var vitals = GameObject.Instantiate(vitalsObj);
-                    vitals.transform.position = new Vector3(-17.2812f, 0.1971f, 0f);
-                    
-                    // Désactiver l'admin
-                    Instance.Log.LogDebug("Disabling admin...");
-                    admin.GetComponent<CircleCollider2D>().enabled = false;
-                    
-                    // Désactiver l'animation car il n'y a plus de carte, donc pour le RP faut plus qu'on la voie
-                    animation.active = false;
-                    
-                    Instance.Log.LogDebug("Done !");
-                    patched = true;
+                    impostorDetector.GetComponent<CircleCollider2D>().enabled = false;
                 }
+                
+                // Relier toutes les vents à gauche, et toutes celles à droite
+                Instance.Log.LogDebug("Rerouting vents...");
+                // TODO: faire ça après
+                
+                Instance.Log.LogInfo("Successfully patched TheSkeld !");
+                Destroy(this.gameObject);
+            }
+        }
+
+        public class MiraHQPatcher : MonoBehaviour
+        {
+            private void FixedUpdate()
+            {
+                var client = AmongUsClient.Instance;
+                
+                // On charge Polus 
+                var res = Addressables.LoadAssetAsync<GameObject>(client.ShipPrefabs[(Index) (int) ShipStatus.MapType.Pb]).Result;
+                if (!res)
+                    return; // On réessaiera de charger à la prochaine update
+                
+                Instance.Log.LogDebug("Patching MiraHQ...");
+                
+                // Récup les ressources
+                var impostorDetectors = GameObject.FindObjectsOfType<ImpostorDetector>();
+
+                // HACK: je sais pas trop à quoi il sert en temps normal, mais ce composant cause le bug de glissement
+                // à la tâche des feuilles !
+                Instance.Log.LogDebug("Fixing \"Clean O₂ Filter\" task...");
+                foreach (var impostorDetector in impostorDetectors)
+                {
+                    impostorDetector.GetComponent<CircleCollider2D>().enabled = false;
+                }
+                
+                Instance.Log.LogInfo("Successfully patched MiraHQ !");
+                Destroy(this.gameObject);
             }
         }
 
@@ -74,7 +125,15 @@ namespace BetterSkeld
         public override void Load()
         {
             Instance = this;
+            
+            ClassInjector.RegisterTypeInIl2Cpp<MiraHQPatcher>();
+            ClassInjector.RegisterTypeInIl2Cpp<SkeldPatcher>();
             new Harmony(PluginInfo.PLUGIN_GUID).PatchAll();
+            
+            SceneManager.sceneLoaded += (Action<Scene, LoadSceneMode>) ((scene, loadSceneMode) =>
+            {
+                ModManager.Instance.ShowModStamp();
+            });
         }
     }
 }
